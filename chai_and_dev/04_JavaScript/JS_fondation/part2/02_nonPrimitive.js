4616bbc4848bb7bb5ca651e47f282886a49a771e
Shantanu <shantanumitra1712@gmail.com>
2026-09-02 14:03:34 +0530

    Add condition challenges for number comparison, string equality, and array checks

--- chai_and_dev/04_JavaScript/JS_fondation/part2/02_nonPrimitive.js
+++ chai_and_dev/04_JavaScript/JS_fondation/part2/02_nonPrimitive.js
@@ -21,3 +21,23 @@ console.log(username.lastName)
 console.log(username)
 
 // console.log(typeof username)
+
+let taoday = new Date()
+// console.log(taoday.getDate())
+
+// Array
+
+// let heros = ["a","b","c",true]
+let anotherUser = ["Shantanu", "Mitra", true]
+
+// console.log(anotherUser[1])
+
+// console.log(1 + "1")
+console.log("1" + 1)
+
+// let isValue = true
+// console.log(isValue + 1)
+
+let isValue = "2abc"
+// console.log(Number(isValue)) // Output will be NaN
+console.log(typeof Number(isValue))
---
+++ chai_and_dev/04_JavaScript/JS_fondation/part3/condition-challenges.js
@@ -0,0 +1,61 @@
+// Check if a number is greater than another number.
+/*
+let num1 = 5
+let num2 = 8
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2 .")
+} else {
+  console.log("num2 is greater than num1 .")
+}
+*/
+
+// checking if a string is equal to another string:
+
+// let username = "shan"
+// let anotherUsername = "shan"
+
+// if (username == anotherUsername) {
+//   console.log("Pick another Username")
+// } else {
+//   console.log("You can pick this username")
+// }
+
+// Check if a variable is a  number or not:
+
+// let score = "44"
+// if (typeof score === "number") {
+//   console.log("Yep, this is a number")
+// } else {
+//   console.log("No that is not a number")
+// }
+
+let credits = []
+
+if (typeof credits == "object") {
+  //   console.log("yep, this is a number")
+} else {
+  console.log("")
+}
+
+// Check if a boolean value is true or false:
+
+let isTeaReady = false
+
+if (isTeaReady) {
+  console.log("Tea is Ready")
+} else {
+  console.log("Tea is not ready ")
+}
+
+// check if array is empty or not
+
+let items = []
+
+console.log(items.length)
+
+if (items.length === 0) {
+  console.log("Array is empty")
+} else {
+  console.log("Array is not empty")
+}
