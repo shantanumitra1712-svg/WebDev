3c2ed23a33f2addb9e3d5ee584210b518fa3beb1
Shantanu <shantanumitra1712@gmail.com>
2026-09-05 23:52:43 +0530

    Add prototype inheritance examples for computer and car objects

---
+++ chai_and_dev/04_JavaScript/JS_fondation/part6/prototype.js
@@ -0,0 +1,22 @@
+let computer = {
+  cpu: 12,
+  ram: 16
+}
+let lenevo = {
+  screen: "HD",
+  __proto__: computer,
+}
+let tomHardware = {}
+
+// console.log(`lenevo`, lenevo.__proto__)
+
+let genericCar = { tyres: 4 }
+
+let tesla = {
+  driver: "AI"
+}
+
+Object.setPrototypeOf(tesla, genericCar)
+
+console.log(`tesla`, Object.getPrototypeOf(tesla));
+
