f893fb57b76a7900401ecb588a2d0d5b04572a12
Shantanu <shantanumitra1712@gmail.com>
2026-09-07 23:27:42 +0530

    Add initial HTML and JavaScript files for JS demo

---
+++ chai_and_dev/04_JavaScript/JS_fondation/part7/script.js
@@ -0,0 +1,6 @@
+console.log("Hello from script");
+// console.log(Window);
+console.log(document.getElementsByTagName("h1"));
+
+// DOM(document object model) -> all of a document elements (h1,body,nav,etc.)
+// BOM(browser object model) -> screen , navigator , location
