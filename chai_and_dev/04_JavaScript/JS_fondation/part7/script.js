b2e1abf7deff2ecf26a1bf1d445738c9e79796e4
Shantanu <shantanumitra1712@gmail.com>
2026-09-08 23:21:51 +0530

    updated some files

--- chai_and_dev/04_JavaScript/JS_fondation/part7/script.js
+++ chai_and_dev/04_JavaScript/JS_fondation/part7/script.js
@@ -1,6 +1,8 @@
 console.log("Hello from script");
 // console.log(Window);
 console.log(document.getElementsByTagName("h1"));
+console.log(window);
+
 
 // DOM(document object model) -> all of a document elements (h1,body,nav,etc.)
 // BOM(browser object model) -> screen , navigator , location
