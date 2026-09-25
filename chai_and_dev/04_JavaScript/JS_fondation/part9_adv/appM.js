e76e2efd2a09f03f687a2723a757f16a3b0c242d
Shantanu <shantanumitra1712@gmail.com>
2026-09-25 12:16:05 +0530

    feat: add math operations module and initial app structure

---
+++ chai_and_dev/04_JavaScript/JS_fondation/part9_adv/appM.js
@@ -0,0 +1,10 @@
+// Module import file 
+
+// default import 
+import multiply from "./mathOperationsM.js";
+console.log(multiply(2, 6));
+
+// named import 
+import { add } from "./mathOperationsM.js";
+
+console.log(add(2, 2));
